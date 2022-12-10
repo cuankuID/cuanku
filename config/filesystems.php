abed00e14d5a6b57eefe0f524180b4a38fa400b0
Farras Arkan <farras.arkan@student.uns.ac.id>
2022-12-10 08:03:11 +0700

    fixing storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -67,7 +67,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        app()->basePath('public_html/storage') => storage_path('app/public'),
     ],
 
 ];
