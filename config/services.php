18ff49ee458290d1ac0348015aa0b1e5d77296dd
Farras Arkan <farras.arkan@student.uns.ac.id>
2023-03-10 00:37:41 +0700

    fix google auth setting

--- config/services.php
+++ config/services.php
@@ -33,6 +33,7 @@ return [
     'google' => [
     'client_id' => env('GOOGLE_CLIENT_ID'),
     'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-    'redirect' => 'http://127.0.0.1:8000/auth/google/call-back',
+    // 'redirect' => 'http://127.0.0.1:8000/auth/google/call-back',
+    'redirect' => 'https://cuanku.space/auth/google/call-back',
     ],
 ];
